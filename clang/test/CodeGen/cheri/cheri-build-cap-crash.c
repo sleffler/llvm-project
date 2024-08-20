b83c2103cab92abed8070d8b37e06d0ede1b5324
Petr Vesely <petr.vesely@codasip.com>
2024-08-20 19:24:32 +0200

    [CHERI] Temporarily disable -Wunsafe-buffer-usage as it causes crashes

---
+++ clang/test/CodeGen/cheri/cheri-build-cap-crash.c
@@ -0,0 +1,13 @@
+// RUN-NOT: %cheri_purecap_cc1 %s -o - -emit-llvm -O1 -Wunsafe-buffer-usage -verify | FileCheck %s
+// TODO - This is crashing when -Wunsafe-buffer-usage is used
+// Likely introduced with this commit: a046d1877201
+// expected-no-diagnostics
+
+#include <cheriintrin.h>
+
+void test(void *__capability cap, char *__capability cap2);
+void test(void *__capability cap, char *__capability cap2) {
+// CHECK-LABEL: @test(
+// CHECK-NEXT:  entry:
+    cheri_cap_build(cap, (__intcap_t)cap2);
+}
