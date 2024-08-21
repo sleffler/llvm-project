5b5e3eb1c88787ca98cd6a78b675e2797ea47698
Petr Vesely <petr.vesely@codasip.com>
2024-08-21 15:05:19 +0200

    [CHERI] Don't report unsupported test as a failure

--- clang/test/CodeGen/cheri/cheri-build-cap-crash.c
+++ clang/test/CodeGen/cheri/cheri-build-cap-crash.c
@@ -1,4 +1,5 @@
-// RUN-NOT: %cheri_purecap_cc1 %s -o - -emit-llvm -O1 -Wunsafe-buffer-usage -verify | FileCheck %s
+// UNSUPPORTED: true
+// RUN: %cheri_purecap_cc1 %s -o - -emit-llvm -O1 -Wunsafe-buffer-usage -verify | FileCheck %s
 // TODO - This is crashing when -Wunsafe-buffer-usage is used
 // Likely introduced with this commit: a046d1877201
 // expected-no-diagnostics
